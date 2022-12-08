409762c81221397d4cca966ed24f1fcc4356d996
arjundesguerra <arjundesguerra@gmail.com>
2022-12-08 02:19:49 -0800

    added functionality for innerdivs to change size

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 const div = document.querySelector('#grid');
 let innerdiv;
 
-//creates grid of 16x16 sqaurres
-createGrid(16,16);
+//creates grid
+createGrid(16, 16);
 function createGrid(rows, columns) {
-    for(let i = 0; i < rows; i++) {
+    for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
-        innerdiv = document.createElement('div');
-        innerdiv.id = ('innerdiv');
-        div.appendChild(innerdiv)
-        //listens for a mouseover a div, then turns the background color black
-        innerdiv.addEventListener("mouseover", function() {
-            this.style.backgroundColor = 'black';
-        });
-        }   
-    }   
+            innerdiv = document.createElement('div');
+            innerdiv.id = ('innerdiv');
+            div.appendChild(innerdiv)
+            innerdiv.style.cssText = `height: calc(640px/${rows}); width: calc(640px/${columns});`;
+            //listens for a mouseover a div, then turns the background color black
+            innerdiv.addEventListener("mouseover", function () {
+                this.style.backgroundColor = 'black';
+            });
+        }
+    }
 }
 
-
- 
