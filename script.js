f7d42241f4dfd7f17e0251bdc0b451e0ec4965ec
arjundesguerra <arjundesguerra@gmail.com>
2022-12-08 01:11:45 -0800

    functioning event listener

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 //div that holds all boxes
 const div = document.querySelector('#grid');
+let innerdiv;
 
-function createGrid(num1, num2) {
-    let prod = num1 * num2;
-    for(let i = 0; i < prod; i++) {
-        const innerdiv = document.createElement('div');
-        innerdiv.classList.add('innerdiv');
+//creates grid of 16x16 sqaurres
+createGrid(16,16);
+function createGrid(rows, columns) {
+    for(let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+        innerdiv = document.createElement('div');
+        innerdiv.id = ('innerdiv');
         div.appendChild(innerdiv)
-    }
+        //listens for a mouseover a div, then turns the background color black
+        innerdiv.addEventListener("mouseover", function() {
+            this.style.backgroundColor = 'black';
+        });
+        }   
+    }   
 }
+
 
  
